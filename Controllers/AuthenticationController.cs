c6a4400f9af96f0d31462b9e22ae155f8d9c029c
aprialap <44420829+aprialap@users.noreply.github.com>
2022-03-17 16:12:34 +0700

    initial commit

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace WebServiceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        public readonly IConfiguration _configuration;
+        public ResponseHelperController _response{ get; }
+
+        public AuthenticationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _response = new ResponseHelperController();
+           //INAC.Model.FinanceModel.InsertUpdateExpense()
+            
+        }
+
+        [HttpGet]
+        public string list()
+        {
+
+            string query = @"SELECT * FROM Authentication";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("AppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+         return _response.Response_Object(table, "200", "success");
+        }
+
+        [HttpGet("detail")]
+        public string getDetail(string username, string password)
+        {
+            string query = @"SELECT * FROM Authentication where Username = '"+username+"' and Password = '"+password+"'";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("AppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                 myCon.Open();
+                 using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                 {
+                    
+                     myReader = myCommand.ExecuteReader();
+                     table.Load(myReader); 
+                     myReader.Close();
+                     myCon.Close();
+                }
+            } 
+            return _response.Response_FirstObject(table, "200", "success");
+        }
+    }
+}
---
+++ Controllers/ResponseHelperController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Data;
+using System.Text;
+
+namespace WebServiceAPI.Controllers
+{
+    public class ResponseHelperController
+    {
+        public string Response_Object(DataTable dt, string code, string msg)
+        {
+            var JSON_String = new StringBuilder();
+            if (dt.Rows.Count > 0)
+            {
+                JSON_String.Append("{");
+                JSON_String.Append("\"code\" : \"" + code + "\",");
+                JSON_String.Append("\"message\" : \"" + msg + "\",");
+                JSON_String.Append("\"data\" : ");
+                JSON_String.Append("[");
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    JSON_String.Append("{");
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                    {
+                        if (j < dt.Columns.Count - 1)
+                        {
+                            JSON_String.Append("\"" + dt.Columns[j].ColumnName.ToString() + "\":" + "\"" + dt.Rows[i][j].ToString() + "\",");
+                        }
+                        else if (j == dt.Columns.Count - 1)
+                        {
+                            JSON_String.Append("\"" + dt.Columns[j].ColumnName.ToString() + "\":" + "\"" + dt.Rows[i][j].ToString() + "\"");
+                        }
+                    }
+                    if (i == dt.Rows.Count - 1)
+                    {
+                        JSON_String.Append("}");
+                    }
+                    else
+                    {
+                        JSON_String.Append("},");
+                    }
+                }
+                JSON_String.Append("]}");
+            }
+            return JSON_String.ToString();
+        }
+
+        public  string Response_FirstObject(DataTable dt, string code, string msg)
+        {
+            var JSON_String = new StringBuilder();
+            if (dt.Rows.Count > 0)
+            {
+                JSON_String.Append("{");
+                JSON_String.Append("\"code\" : \"" + code + "\",");
+                JSON_String.Append("\"message\" : \"" + msg + "\",");
+                JSON_String.Append("\"data\" : ");
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    JSON_String.Append("{");
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                    {
+                        if (j < dt.Columns.Count - 1)
+                        {
+                            JSON_String.Append("\"" + dt.Columns[j].ColumnName.ToString() + "\":" + "\"" + dt.Rows[i][j].ToString() + "\",");
+                        }
+                        else if (j == dt.Columns.Count - 1)
+                        {
+                            JSON_String.Append("\"" + dt.Columns[j].ColumnName.ToString() + "\":" + "\"" + dt.Rows[i][j].ToString() + "\"");
+                        }
+                    }
+                    if (i == dt.Rows.Count - 1)
+                    {
+                        JSON_String.Append("}");
+                    }
+                    else
+                    {
+                        JSON_String.Append("},");
+                    }
+                }
+                JSON_String.Append("}");
+            }
+            return JSON_String.ToString();
+        }
+
+    }
+}
